533e5c97545d6417126f65ddaa9a9254945e2246
Dulce Jaramillo <dulcejaramillo@dulces-MacBook-Pro.local>
2023-08-19 17:50:22 -0700

    five functions

---
+++ index.js
@@ -0,0 +1,46 @@
+// 1.
+
+// const ageCheck = (yearBorn)=>{
+//     age = 2023- yearBorn
+//     console.log (age)
+// }
+// ageCheck(1981)
+
+// 2. 
+
+// const isClean = (cleanOrNot)=>{
+//     if (chores = cleanOrNot){
+//         console.log("House is Clean")
+//     }else{
+//         console.log("clean your house")
+//     }
+// }
+// isClean(true)
+// // 3.
+// const hasMembership =(membership, goodstanding)=>{
+//     if (membership == "paid" && goodstanding ==true){
+//         console.log ("Welcome Member");
+//     }else{
+//         console.log("something is wrong with you")
+//     }
+// }
+// hasMembership("paid",false)
+// 4.
+// const onTime =(time)=>{
+//     if (time=> 9){
+//         console.log ("you are late!")
+//     }else{
+//         console.log("way to be on time")
+//     }
+// }
+// onTime(9.01)
+// 5.
+const tickets =(price)=>{
+    if (price > 200){
+        console.log("Too Expensive");
+    }else{
+        console.log("quick, buy them now!")
+    }
+}
+tickets(150)
+
